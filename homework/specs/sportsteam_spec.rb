18ff07f96c088e4093de7b0356d8200869df906e
davina <davina.kuhnel@gmail.com>
2018-04-16 16:52:12 +0100

    final commit

--- homework/specs/sportsteam_spec.rb
+++ homework/specs/sportsteam_spec.rb
@@ -10,15 +10,37 @@ def setup
 end
 
   def test_team_name
-    assert_equal("Manchester United", @sports_team.get_team_name())
+    assert_equal("Manchester United", @sports_team.name())
   end
 
   def test_players
-    assert_equal(["Carrick", "Giggs", "Scholes"], @sports_team.get_players())
+    assert_equal(["Carrick", "Giggs", "Scholes"], @sports_team.players())
   end
 
   def test_coach
-    assert_equal("Ferguson", @sports_team.get_coach())
+    assert_equal("Ferguson", @sports_team.coach())
+  end
+
+  def test_set_coach_new_name
+    @sports_team.set_coach_name("Sanders")
+    assert_equal("Sanders", @sports_team.coach())
+  end
+
+  def test_add_player
+    @sports_team.add_player("Davina")
+    assert_equal(["Carrick", "Giggs", "Scholes", "Davina"], @sports_team.players())
+  end
+
+  def test_does_player_exist
+    assert_equal(true, @sports_team.does_player_exist("Carrick"))
+  end
+
+  def test_does_player_exist_no
+    assert_equal(false, @sports_team.does_player_exist("Davina"))
+  end
+
+  def test_count_points
+    assert_equal(1, @sports_team.count_points(1))
   end
 
 end
--- homework/sportsteam.rb
+++ homework/sportsteam.rb
@@ -3,26 +3,49 @@
 
 
 class SportsTeam
-
+attr_reader(:name, :players, :coach, :points)
 
   def initialize (name, players, coach)
     @name = name
     @players = players
     @coach = coach
+    @points = 0
   end
 
-  def get_team_name
-    @name
+  # def get_team_name
+  #   @name
+  # end
+  #
+  # def get_players
+  #   @players
+  # end
+  #
+  # def get_coach
+  #   @coach
+  # end
+
+  def set_coach_name(new_name)
+    @coach = new_name
   end
 
-  def get_players
-    @players
+  def add_player(new_player)
+    @players.push(new_player)
   end
 
-  def get_coach
-    @coach
+  def does_player_exist(check_player)
+    for player in @players
+      if player == check_player
+        return true
+      else
+        return false
+      end
+    end
   end
 
-
+  def count_points(wins)
+    if wins >= 0
+    @points += wins
+  end
+end
 
 end
